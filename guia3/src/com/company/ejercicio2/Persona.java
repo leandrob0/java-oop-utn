2e25eb5f2ce72651cf831db301f249dab6e14a22
leandrob0 <leandro.bovino@gmail.com>
2022-04-07 20:27:01 -0300

    ejercicio 2 hecho

--- guia3/src/com/company/ejercicio2/Persona.java
+++ guia3/src/com/company/ejercicio2/Persona.java
@@ -1,4 +1,68 @@
 package com.company.ejercicio2;
 
 public class Persona {
+    protected String dni;
+    protected String first;
+    protected String last;
+    protected String email;
+    protected String direccion;
+
+    public Persona(String dni, String first, String last, String email, String direccion) {
+        this.dni = dni;
+        this.first = first;
+        this.last = last;
+        this.email = email;
+        this.direccion = direccion;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "dni='" + dni + '\'' +
+                ", first='" + first + '\'' +
+                ", last='" + last + '\'' +
+                ", email='" + email + '\'' +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
 }
--- guia3/src/com/company/ejercicio2/Staff.java
+++ guia3/src/com/company/ejercicio2/Staff.java
@@ -1,4 +1,41 @@
 package com.company.ejercicio2;
 
-public class Staff {
+public class Staff extends Persona {
+    protected double salario;
+    protected String turno;
+
+    public Staff(String dni, String first,String last,String email, String direccion,double salario, String turno) {
+        super(dni,first,last,email,direccion);
+        this.salario = salario;
+        this.turno = turno;
+    }
+
+    public double salarioAnual() {
+        return salario*12;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    @Override
+    public String toString() {
+        return "Staff{" +
+                super.toString() +
+                "salario=" + salario +
+                ", turno='" + turno + '\'' +
+                '}';
+    }
 }
