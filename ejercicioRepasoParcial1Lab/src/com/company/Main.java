3efff11fb60d3b11ace3c59d0887e550a68b64f8
leandrob0 <leandro.bovino@gmail.com>
2022-04-28 19:44:50 -0300

    ejercicio de repaso proyecto creado, pensamiento hecho en el main.

---
+++ ejercicioRepasoParcial1Lab/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // super clase de diario, revista y libro. con codigo,titulo (o titular en diarios y revistas), y anio de publicacion.
+
+        // los libros pueden ser de distintos generos, novelas, cuentos, etc. con metodos de alquilar(), devolver(), y alquilado().
+        // las revistas tambien tienen los metodos de alquilar(), devolver(), y alquilado().
+        // los diarios solo se pueden leer en la biblioteca y Roberto quiere saber si el diario se esta leyendo o no.
+
+        // voy a hacer una interfaz que va a implementarse en las 3 clases, con los metodos que se piden, asi puedo
+        // sobreescribirlos en cada uno e implementar lo que se pide facil.
+        /*Mostrar errores si se quiere alquilar un libro alquilado
+        Mostrar errores si se quiere devolver una revista devuelta
+        Mostrar errores si se esta leyendo un diario
+
+        Mostrar un listado de libros, revistas y diarios disponibles y no disponibles.*/
+    }
+}
